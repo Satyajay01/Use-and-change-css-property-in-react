2c36969cf916fc8be00003f3bcf63c68fceff026
satyajay01 <satyajaydibya0@gmail.com>
2024-03-05 17:42:51 +0530

    add new h1 tag and css elements

--- src/Components/Change_css_property.jsx
+++ src/Components/Change_css_property.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import './style.css'
 
 function Change_css_property() {
     const [myColor, setmyColor] =useState("blue")
@@ -16,17 +17,16 @@ function Change_css_property() {
     const divStyle = {
         border: '2px solid red',
         height: '300px', 
-        width: '500px' 
+        width: '100vw' 
       };
 
-
-      
 
   return (
    <>
     <div style={divStyle}>
+        <h1 style={{ color: 'red',backgroundColor: "lightblue"}}>hii this is is Satyajay</h1>
     <button onClick={ColorFun}>Button</button>
-    <h1 style={{color: myColor,fontSize: fontSize, backgroundColor: bgcolor, display: Displya_pro}}>Change_css_property test</h1>
+    <h1 className='my_text' style={{color: myColor,fontSize: fontSize, backgroundColor: bgcolor, display: Displya_pro}}>Change_css_property test</h1>
 
     </div>
    
